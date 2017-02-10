4fba5478955519e26f9e982dca8b976a7d56e18e
vlmeg <vlmeg@users.noreply.github.com>
2017-02-10 22:34:40 +0100

    Create Plot2d.py

---
+++ Plot2d.py
@@ -0,0 +1,20 @@
+# This method picks a bunch of random samples (images in your case) to plot onto the chart:
+
+def Plot2D(T, title, x, y, num_to_plot=40):
+  fig = plt.figure()
+  ax = fig.add_subplot(111)
+  ax.set_title(title)
+  ax.set_xlabel('Component: {0}'.format(x))
+  ax.set_ylabel('Component: {0}'.format(y))
+  x_size = (max(T[:,x]) - min(T[:,x])) * 0.08
+  y_size = (max(T[:,y]) - min(T[:,y])) * 0.08
+  for i in range(num_to_plot):
+    img_num = int(random.random() * num_images)
+    x0, y0 = T[img_num,x]-x_size/2., T[img_num,y]-y_size/2.
+    x1, y1 = T[img_num,x]+x_size/2., T[img_num,y]+y_size/2.
+    img = df.iloc[img_num,:].reshape(num_pixels, num_pixels)
+    ax.imshow(img, aspect='auto', cmap=plt.cm.gray, interpolation='nearest', zorder=100000, extent=(x0, x1, y0, y1))
+    
+    
+Plot2D(Z, "PCA transformed data", 1, 2, num_to_plot = 40)
+#Z= float 64 size (...,3) transformed by PSA
